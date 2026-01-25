ebce733c338e54b77569f900c58093e76521e241
pulishashank8 <157274505+pulishashank8@users.noreply.github.com>
2026-01-25 09:43:36 +0000

    fix update

--- .next/dev/static/chunks/src_app_dashboard_page_tsx_903956ca._.js
+++
@@ -1,7 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/_33ac09aa._.js"
-],
-    source: "dynamic"
-});
